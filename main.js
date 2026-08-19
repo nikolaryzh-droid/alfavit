e1c2993cb7ba119f80b992531488baa102488ebb
nikolaryzh-droid <nikolaryzh@gmail.com>
2026-08-19 15:32:09 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
-document.getElementById('showMore')?.addEventListener('click', function(){
- const block=document.getElementById('moreWorks');
- const shown=block.classList.toggle('show');
- this.textContent=shown?'Скрыть дополнительные работы':'Показать больше работ';
+const button = document.getElementById('showMore');
+const block = document.getElementById('moreWorks');
+button?.addEventListener('click', () => {
+  const active = block.classList.toggle('show');
+  button.textContent = active ? 'Скрыть' : 'Показать все';
 });
